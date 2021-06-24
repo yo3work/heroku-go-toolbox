cfa9a0dacb30ff693fd481af6762cdd3e8c8b6f8
Akira Yoshioka <akira.yoshioka@desamis.co.jp>
2021-06-25 03:36:58 +0900

    renewal site

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	"image/color"
 	"image/png"
 	"io/ioutil"
-	"math/rand"
 	"strings"
 
 	"golang.org/x/image/font"
@@ -39,149 +38,169 @@ func generateEmoji(EmojiText string, DownloadDirectory string) []string {
 	// テキストファイルの読み込み,\rが含まれている場合があるので置換しておく
 	f := EmojiText
 	f = strings.Replace(f, "\r", "", -1)
+	f = strings.Replace(f, " ", "", -1)
+	f = strings.Replace(f, "　", "", -1)
 	s := strings.Split(f, "\n")
 
-	cnt := 0
+	cnt := 1
+	filenameCount := 1
 	// テキストファイルを行ごとに読み込んでループ
 	for _, str := range s {
 		Message := str
-		OutputFileName := DownloadDirectory + "/" + strconv.Itoa(cnt) + ".png"
-
-		// カンマ区切りなら後半をファイル名にする
-		if strings.Contains(Message, ",") {
-			slice := strings.Split(Message, ",")
-			Message = slice[0]
-			OutputFileName = DownloadDirectory + "/" + slice[1] + ".png"
+		if Message == "" {
+			continue
 		}
 
-		// 画像サイズを決める
-		img := image.NewRGBA(image.Rect(0, 0, 128, 128))
+		for i := 0; i < 6; i++ {
+			Message = str
+			OutputFileName := DownloadDirectory + "/" + strconv.Itoa(filenameCount) + ".png"
 
-		// 画像背景色を決める
-		for y := img.Rect.Min.Y; y < img.Rect.Max.Y; y++ {
-			for x := img.Rect.Min.X; x < img.Rect.Max.X; x++ {
-				img.Set(x, y, color.RGBA{0, 0, 0, 0})
+			// カンマ区切りなら後半をファイル名にする
+			if strings.Contains(Message, ",") {
+				slice := strings.Split(Message, ",")
+				Message = slice[0]
+				OutputFileName = DownloadDirectory + "/" + slice[1] + strconv.Itoa(cnt) + ".png"
 			}
-		}
 
-		// フォントファイルの読み込みとパース
-		ftBin, err := ioutil.ReadFile("static/fonts/NotoSansCJKjp-Bold.otf")
-		if err != nil {
-			log.Fatalf("failed to load font: %s", err.Error())
-		}
-		ft, err := opentype.Parse(ftBin)
-		if err != nil {
-			log.Fatalf("failed to parse font: %s", err.Error())
-		}
+			// 画像サイズを決める
+			img := image.NewRGBA(image.Rect(0, 0, 128, 128))
 
-		// 文字数を判定する
-		if len(Message)/len([]rune(Message)) != 3 {
-			fmt.Fprintln(logfile, Message+" ERROR[ごめんなさい！全角文字を使ってね！]")
-			continue
-		}
+			// 画像背景色を決める
+			for y := img.Rect.Min.Y; y < img.Rect.Max.Y; y++ {
+				for x := img.Rect.Min.X; x < img.Rect.Max.X; x++ {
+					img.Set(x, y, color.RGBA{0, 0, 0, 0})
+				}
+			}
 
-		// 文字数による描画設定(フォントサイズ、行数、描画座標)
-		var CharSize float64 = 64
-		CharSize = 64
-		Row1Xvalue, Row1Yvalue, Row2Xvalue, Row2Yvalue := 0, 0, 0, 0
-		Row1Text, Row2Text := "", ""
-		if len([]rune(Message)) == 2 {
+			// フォントファイルの読み込みとパース
+			ftBin, err := ioutil.ReadFile("static/fonts/NotoSansCJKjp-Bold.otf")
+			if err != nil {
+				log.Fatalf("failed to load font: %s", err.Error())
+			}
+			ft, err := opentype.Parse(ftBin)
+			if err != nil {
+				log.Fatalf("failed to parse font: %s", err.Error())
+			}
+
+			// 文字数を判定する
+			if len(Message)/len([]rune(Message)) != 3 {
+				fmt.Fprintln(logfile, Message+" ERROR[ごめんなさい！全角文字を使ってね！]")
+				continue
+			}
+
+			// 文字数による描画設定(フォントサイズ、行数、描画座標)
+			var CharSize float64 = 64
 			CharSize = 64
-			Row1Xvalue = 0 * 64
-			Row1Yvalue = 85 * 64
-			Row1Text = Message
-		} else if len([]rune(Message)) == 3 {
-			CharSize = 42
-			Row1Xvalue = 0 * 64
-			Row1Yvalue = 75 * 64
-			Row1Text = Message
-		} else if len([]rune(Message)) == 4 {
-			CharSize = 64
-			Row1Xvalue = 0 * 64
-			Row1Yvalue = 60 * 64
-			Row2Xvalue = 0 * 64
-			Row2Yvalue = 120 * 64
-			Row1Text = Message[0:6]
-			Row2Text = Message[6:12]
-		} else if len([]rune(Message)) == 6 {
-			CharSize = 42
-			Row1Xvalue = 0 * 64
-			Row1Yvalue = 55 * 64
-			Row2Xvalue = 0 * 64
-			Row2Yvalue = 105 * 64
-			Row1Text = Message[0:9]
-			Row2Text = Message[9:18]
-		} else if len([]rune(Message)) == 8 {
-			CharSize = 32
-			Row1Xvalue = 0 * 64
-			Row1Yvalue = 50 * 64
-			Row2Xvalue = 0 * 64
-			Row2Yvalue = 100 * 64
-			Row1Text = Message[0:12]
-			Row2Text = Message[12:24]
-		} else {
-			fmt.Fprintln(logfile, Message+" ERROR[ごめんなさい！全角2,3,4,6,8文字以外は対応してません！]")
-			continue
-		}
+			Row1Xvalue, Row1Yvalue, Row2Xvalue, Row2Yvalue := 0, 0, 0, 0
+			Row1Text, Row2Text := "", ""
+			if len([]rune(Message)) == 2 {
+				CharSize = 64
+				Row1Xvalue = 0 * 64
+				Row1Yvalue = 85 * 64
+				Row1Text = Message
+			} else if len([]rune(Message)) == 3 {
+				CharSize = 42
+				Row1Xvalue = 0 * 64
+				Row1Yvalue = 75 * 64
+				Row1Text = Message
+			} else if len([]rune(Message)) == 4 {
+				CharSize = 64
+				Row1Xvalue = 0 * 64
+				Row1Yvalue = 60 * 64
+				Row2Xvalue = 0 * 64
+				Row2Yvalue = 120 * 64
+				Row1Text = Message[0:6]
+				Row2Text = Message[6:12]
+			} else if len([]rune(Message)) == 6 {
+				CharSize = 42
+				Row1Xvalue = 0 * 64
+				Row1Yvalue = 55 * 64
+				Row2Xvalue = 0 * 64
+				Row2Yvalue = 105 * 64
+				Row1Text = Message[0:9]
+				Row2Text = Message[9:18]
+			} else if len([]rune(Message)) == 8 {
+				CharSize = 32
+				Row1Xvalue = 0 * 64
+				Row1Yvalue = 50 * 64
+				Row2Xvalue = 0 * 64
+				Row2Yvalue = 100 * 64
+				Row1Text = Message[0:12]
+				Row2Text = Message[12:24]
+			} else {
+				fmt.Fprintln(logfile, Message+" ERROR[ごめんなさい！全角2,3,4,6,8文字以外は対応してません！]")
+				continue
+			}
 
-		// 文字色の候補
-		color := []*color.RGBA{
-			{255, 0, 0, 255},
-			{255, 219, 0, 255},
-			{73, 255, 0, 255},
-			{0, 255, 146, 255},
-			{0, 146, 255, 255},
-			{73, 0, 255, 255},
-			{255, 0, 219, 255},
-		}
+			// 文字色の候補
+			color := []*color.RGBA{
+				{54, 197, 240, 255},
+				{46, 182, 125, 255},
+				{224, 30, 90, 255},
+				{236, 178, 46, 255},
+				{74, 21, 75, 255},
+				{0, 0, 0, 255},
+			}
 
-		// 文字色をランダムにあてがう
-		rand.Seed(time.Now().UnixNano())
-		RandomNumber := rand.Intn(7)
+			// 文字色をランダムにあてがう
+			//rand.Seed(time.Now().UnixNano())
+			//RandomNumber := rand.Intn(6)
 
-		opt := opentype.FaceOptions{
-			Size:    CharSize,
-			DPI:     72,
-			Hinting: font.HintingNone,
-		}
-		face, err := opentype.NewFace(ft, &opt)
+			opt := opentype.FaceOptions{
+				Size:    CharSize,
+				DPI:     72,
+				Hinting: font.HintingNone,
+			}
+			face, err := opentype.NewFace(ft, &opt)
 
-		// 1行目の描画
-		d := &font.Drawer{
-			Dst:  img,
-			Src:  image.NewUniform(color[RandomNumber]),
-			Face: face,
-			Dot:  fixed.Point26_6{fixed.Int26_6(Row1Xvalue), fixed.Int26_6(Row1Yvalue)},
-		}
-		d.DrawString(Row1Text)
-
-		// 2行目の描画
-		if len([]rune(Message)) >= 4 {
-			d2 := &font.Drawer{
+			// 1行目の描画
+			d := &font.Drawer{
 				Dst:  img,
-				Src:  image.NewUniform(color[RandomNumber]),
+				Src:  image.NewUniform(color[cnt-1]),
 				Face: face,
-				Dot:  fixed.Point26_6{fixed.Int26_6(Row2Xvalue), fixed.Int26_6(Row2Yvalue)},
+				Dot:  fixed.Point26_6{fixed.Int26_6(Row1Xvalue), fixed.Int26_6(Row1Yvalue)},
 			}
-			d2.DrawString(Row2Text)
+			d.DrawString(Row1Text)
+
+			// 2行目の描画
+			if len([]rune(Message)) >= 4 {
+				d2 := &font.Drawer{
+					Dst:  img,
+					Src:  image.NewUniform(color[cnt-1]),
+					Face: face,
+					Dot:  fixed.Point26_6{fixed.Int26_6(Row2Xvalue), fixed.Int26_6(Row2Yvalue)},
+				}
+				d2.DrawString(Row2Text)
+			}
+
+			// ファイル出力
+			file, err := os.Create(OutputFileName)
+			if err != nil {
+				panic(err.Error())
+			}
+			defer file.Close()
+			fmt.Fprintln(logfile, Message+" "+OutputFileName)
+
+			if err := png.Encode(file, img); err != nil {
+				panic(err.Error())
+			}
+
+			//ファイル名をスライスに追加
+			if cnt == 1 {
+				EmojiURL = append(EmojiURL, "row_start")
+				EmojiURL = append(EmojiURL, OutputFileName)
+			} else if cnt == 6 {
+				EmojiURL = append(EmojiURL, OutputFileName)
+				EmojiURL = append(EmojiURL, "row_end")
+				cnt = 0
+			} else {
+				EmojiURL = append(EmojiURL, OutputFileName)
+			}
+
+			cnt++
+			filenameCount++
+
 		}
-
-		// ファイル出力
-		file, err := os.Create(OutputFileName)
-		if err != nil {
-			panic(err.Error())
-		}
-		defer file.Close()
-		fmt.Fprintln(logfile, Message+" "+OutputFileName)
-
-		if err := png.Encode(file, img); err != nil {
-			panic(err.Error())
-		}
-
-		//ファイル名をスライスに追加
-		EmojiURL = append(EmojiURL, OutputFileName)
-
-		cnt++
 	}
 	//    if s.Err() != nil {
 	//        // non-EOF error.
@@ -209,7 +228,7 @@ func handleHome(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func handleInputConfirm(w http.ResponseWriter, req *http.Request) {
+func handleResult(w http.ResponseWriter, req *http.Request) {
 
 	EmojiText := req.FormValue("emoji_text")
 	EmojiTextColor := req.FormValue("emoji_text_color")
@@ -248,7 +267,7 @@ func handleInputConfirm(w http.ResponseWriter, req *http.Request) {
 	}
 
 	t := template.Must(template.ParseFiles(
-		"template/input_confirm.gohtml",
+		"template/result.gohtml",
 	))
 	if err := t.Execute(w, struct {
 		EmojiText      string
@@ -286,7 +305,7 @@ func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 	http.Handle("/tmp/", http.StripPrefix("/tmp/", http.FileServer(http.Dir("tmp/"))))
 	http.HandleFunc("/", handleHome)
-	http.HandleFunc("/input_confirm", handleInputConfirm)
+	http.HandleFunc("/result", handleResult)
 	http.HandleFunc("/secret", handleSecret)
 	log.Printf("ポート %s で待ち受けを開始します...", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
